00b126c9576657834c7e81150844c9fcfe5b7b54
Lewis Davies <lewis2106@gmail.com>
2017-08-18 08:36:46 +0100

    Apply Materialize CSS to all pages

---
+++ output/theme/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('table').addClass('striped');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

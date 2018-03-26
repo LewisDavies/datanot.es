dcc4d3d48336f34d03dccfabb36a158a2ad4c6f6
Lewis Davies <lewis2106@gmail.com>
2018-03-26 21:25:00 +0100

    New article: Working with Missing Values

---
+++ make_site.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+from pelicanconf_static_paths import STATIC_PATHS
+
+# Find all notebooks
+p = Path('content')
+notebooks = list(p.glob('**/*.ipynb'))
+
+# Convert notebooks to Markdown
+for file in notebooks:
+    os.system('jupyter nbconvert {} --to markdown'.format(file))
+    file = str(file).replace('.ipynb', '.md')
+    # Remove first line of file or content won't load
+    with open(file, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(file, 'w') as fout:
+        fout.writelines(data[1:])
+
+# Find directories containing generated images
+img_dirs = [str(d)[8:] for d in list(p.glob('**')) if d.name.endswith('_files')]
+STATIC_PATHS.extend(img_dirs)
+for i in range(len(STATIC_PATHS) - 1):
+    STATIC_PATHS[i] = "    '" + STATIC_PATHS[i] + "',\n"
+STATIC_PATHS[-1] = "    '" + STATIC_PATHS[-1] + "'\n"
+
+# Import base pelican config
+with open('pelicanconf_base.py', 'r') as file:
+    conf = file.read().splitlines(True)
+
+conf.extend(['\n', 'STATIC_PATHS = [\n'])
+for path in STATIC_PATHS:
+    conf.append(path)
+conf.extend([']\n', '\n'])
+
+# Write pelican config with all static paths
+with open('pelicanconf.py', 'w') as file:
+    file.writelines(conf)
+
+# # Compile site
+generate_site = os.system('pelican content -o output -s pelicanconf.py')
+
+# If no error:
+if generate_site == 0:
+    # Print action
+    print('No errors found during site generation, deploying to Github Pages')
+    
+#     # Publish site to github
+    os.system('ghp-import -m "Generate Pelican site" -b gh-pages output -c lewisdavi.es')
+    os.system('git push origin gh-pages')
+    os.system('git push origin master')
+else:
+    # Print error
+    print('Error found during site generation, halting deployment')
+
